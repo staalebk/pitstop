580aff389a9b7f213be66d090a8ec22b82967875
Espen Haviken <espen.haviken@gmail.com>
2024-04-15 02:11:39 +0200

    Changed server to run asyncio so I can do both udp and websockets at the same time

--- server/udp_handler.py
+++ server/udp_handler.py
@@ -1,57 +1,70 @@
 import struct
 import uuid
+import asyncudp
 from collections import deque
-from main import active_senders, addr_to_uuid
+from shared import active_senders
+
+# Contains a mapping from the address of the sender to the UUID of the sender
+addr_to_uuid = {}
 
 
 def parse_data_packet(payload):
     try:
         # Unpack timestamp
-        timestamp, = struct.unpack('>Q', payload[:8])
-        payload = payload[8:]
+        timestamp, = struct.unpack('<Q', payload[1:9])
+        payload = payload[9:]
 
         # Change timestamps from microseconds to milliseconds
         timestamp = timestamp // 1000
 
         # Round to closest 100 ms
         timestamp = (timestamp // 100) * 100
+        print("Data parsing timestamp", timestamp)
 
         # Unpack current data
-        speed, heading, *brake_temp = struct.unpack('>HH' + 'H'*16, payload[:36])
+        speed, heading, *brake_temp = struct.unpack('<HH' + 'H'*16, payload[:36])
         payload = payload[36:]
+        print("Data parsing speed", speed)
+        print("Data parsing heading", heading)
+        print("Data parsing brake_temp", brake_temp)
 
         # Unpack array of the position of the last 40 * 100ms
         positions = []
+        rpms = []
         for _ in range(40):
-            lat, long, rpm = struct.unpack('>ffH', payload[:10])
-            positions.append((lat, long, rpm))
+            lat, long, rpm = struct.unpack('<ffH', payload[:10])
+            positions.append((lat, long))
+            rpms.append(rpm)
             payload = payload[10:]
+
+        print("Data parsing positions", positions)
+        print("Data parsing rpm", rpms)
 
         message = {
             'timestamp': timestamp,
             'speed': speed / 100,  # Convert to km/h
             'heading': heading / 100,  # Convert to degrees
-            'brake_temp_left': [temp / 10 - 100 for temp in brake_temp[:16]],  # Left brake temps to C
-            'brake_temp_right': [temp / 10 - 100 for temp in brake_temp[16:]],  # Right brake temps to C
-            'positions': positions
+            'brake_temp': [temp / 10 - 100 for temp in brake_temp],  # 16 brake temps to C
+            'positions': positions,  # Locations
+            'rpms': rpms  # RPMs
         }
 
         return message
 
-    except struct.error:
-        print("Error parsing message")
+    except struct.error as e:
+        print("Error parsing message:", e)
         return None
 
 
 def parse_auth_packet(payload):
-    uuid_bytes = struct.unpack('16s', payload)[0]
+    uuid_bytes = struct.unpack('16s', payload[1:])[0]
     packet_uuid = uuid.UUID(bytes=uuid_bytes)
     return packet_uuid
 
 
 def is_auth_packet(packet):
-    # An auth packet is 16 bytes long (128 bits)
-    return len(packet) == 16
+    # An auth packet is 17 bytes long (128 bits), one byte for the packet type and 16 bytes for the UUID
+    return packet[0] == 0xFF and len(packet) == 17
 
 
 def handle_data_packet(payload, sender_addr):
@@ -62,34 +75,47 @@ def handle_data_packet(payload, sender_addr):
     sender_uuid = addr_to_uuid[sender_addr]
     data_message = parse_data_packet(payload)
 
-    if active_senders[sender_uuid] is None:
+    if data_message is None:
+        print("Received malformed data packet from", sender_addr)
+        return
+
+    print("Received data from", sender_addr, "containing timestamp ", data_message['timestamp'])
+
+    if active_senders.get(sender_uuid) is None:
         active_senders[sender_uuid] = deque(maxlen=100)
 
     sender_buffer = active_senders[sender_uuid]
 
     # Add the newest data coming in with the packet
     new_sender_entry = {
+        'id': sender_uuid,
         'timestamp': data_message['timestamp'],
         'speed': data_message['speed'],
         'heading': data_message['heading'],
-        'brake_temp_left': data_message['brake_temp_left'],
-        'brake_temp_right': data_message['brake_temp_right'],
-        'position': data_message['positions'][0]
+        'brake_temp': data_message['brake_temp'],
+        'position': data_message['positions'][0],
+        'rpm': data_message['rpms'][0]
     }
 
     sender_buffer.append(new_sender_entry)
 
+    position_and_rpm = zip(data_message['positions'][1:], data_message['rpms'][1:])
+
     # Update any missing positions in older timestamps in sender's buffer
-    for index, position in enumerate(data_message['positions'][1:]):
-        timestamp_for_position = data_message['timestamp'] + ((index + 1) * 100)
+    for index, (position, rpm) in enumerate(position_and_rpm):
+        timestamp_for_entry = data_message['timestamp'] + ((index + 1) * 100)
 
         # If timestamp already exists in buffer, we just skip it
-        if any(entry['timestamp'] == timestamp_for_position for entry in sender_buffer):
+        if any(entry['timestamp'] == timestamp_for_entry for entry in sender_buffer):
             continue
 
+        print("Backfilling",timestamp_for_entry, "with", position, "and", rpm)
+
         buffer_entry = {
-            'timestamp': timestamp_for_position,
-            'position': position
+            'id': sender_uuid,
+            'timestamp': timestamp_for_entry,
+            'position': position,
+            'rpm': rpm
         }
         sender_buffer.appendleft(buffer_entry)
 
@@ -101,13 +127,21 @@ def handle_data_packet(payload, sender_addr):
 
 def handle_auth_packet(payload, sender):
     sender_uuid = parse_auth_packet(payload)
-    print("received auth from", sender, "containing uuid", sender_uuid)
+    print("Received auth from", sender, "containing uuid", sender_uuid)
     addr_to_uuid[sender] = sender_uuid
 
 
 def handle_packet(payload, sender):
-    print("received payload", payload, "from", sender)
     if is_auth_packet(payload):
         handle_auth_packet(payload, sender)
     else:
         handle_data_packet(payload, sender)
+
+
+async def listen_for_udp(hostname, port):
+    sock = await asyncudp.create_socket(local_addr=(hostname, port))
+    print("Listening to UDP on", hostname + ":" + str(port))
+
+    while True:
+        data, addr = await sock.recvfrom()
+        handle_packet(data, addr[0] + ":" + str(addr[1]))
--- server/websocket_push.py
+++ server/websocket_push.py
@@ -1,46 +1,78 @@
 import asyncio
-from websockets.server import serve
 import json
-from main import active_senders
+import time
+
+import websockets
+from websockets.server import serve
+from shared import active_senders
+
+
+# Helper function to get the current time in milliseconds
+def current_milli_time():
+    return int(round(time.time() * 1000))
+
+
+# Add a space as thousand separator in the timestamp
+def format_timestamp(time_num):
+    return "{:,}".format(time_num).replace(",", " ")
 
 
 async def push_data_to_websocket(websocket):
     print("New websocket connection from", websocket.remote_address)
 
+    optimal_timestamp = 0
     # Find newest available timestamp in active_senders
-    optimal_timestamp = max(sender['timestamp'] for sender in active_senders.values())
+    for sender_buffer in active_senders.values():
+        for entry in sender_buffer:
+            if entry['timestamp'] > optimal_timestamp:
+                optimal_timestamp = entry['timestamp']
 
     # Subtract 5 seconds to the millisecond timestamp found above
     optimal_timestamp -= 5000
 
+    print("Optimal timestamp to start at is", format_timestamp(optimal_timestamp), "ms")
+
+    # Compare optimal timestamp to current server time to determine approximate latency
+    # server_timestamp = current_milli_time()
+    # print("Approximate latency is", format_timestamp(server_timestamp - optimal_timestamp), "ms")
+
     # Find the closest timestamp to the optimal timestamp
-    current_timestamp = 0
-    for sender in active_senders.values():
-        for entry in sender:
-            if abs(current_timestamp - entry['timestamp']) < abs(current_timestamp - optimal_timestamp):
+    current_timestamp = None
+    for sender_buffer in active_senders.values():
+        for entry in sender_buffer:
+            if current_timestamp is None or abs(current_timestamp - entry['timestamp']) > abs(current_timestamp - optimal_timestamp):
                 current_timestamp = entry['timestamp']
 
-    print("Starting to send data to", websocket.remote_address, "from timestamp", current_timestamp)
+    print("Starting to send data to", websocket.remote_address, "from timestamp", format_timestamp(current_timestamp))
 
     while True:
+        if websocket.closed:
+            print("Websocket connection closed to", websocket.remote_address)
+            break
+
         payload_to_send = []
 
         # Get each entry from active_senders that match current_timestamp
-        for sender in active_senders.values():
-            for entry in sender:
+        for sender_uuid, sender_buffer in active_senders.items():
+            for entry in sender_buffer:
                 if entry['timestamp'] == current_timestamp:
                     payload_to_send.append(entry)
 
-        # Send the data over the websocket
-        websocket.send(json.dumps(payload_to_send))
-        print("Sent timestamp", current_timestamp, "to", websocket.remote_address, "with", len(payload_to_send), "entries")
+        print("Timestamp to send is", format_timestamp(current_timestamp))
+        if len(payload_to_send) > 0:
+            try:
+                await websocket.send(json.dumps(payload_to_send))
+                print("Sent", len(payload_to_send), "entries to", websocket.remote_address)
+            except websockets.ConnectionClosedError:
+                print("Websocket connection closed to", websocket.remote_address)
+                break
 
         # Wait for a short period before sending the next update
         await asyncio.sleep(0.1)
         current_timestamp += 100
 
 
-async def start_server(hostname, port):
+async def listen_for_websocket(hostname, port):
     print("Listening to websocket", hostname + ":" + str(port))
 
     stop = asyncio.Future()
